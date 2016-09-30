93cab02238a6fa8f67f87ac121b251a2f1580722
robertoti <info.robertolima@gmail.com>
2016-09-30 03:07:29 -0300

    Pagina de reserva de veiculos: Método de conversão de datas para o DB;

--- controllers/reserva.php
+++ controllers/reserva.php
@@ -10,8 +10,13 @@ class Reserva extends Controller {
     public function index() 
     {    
         $this->view->reservaList = $this->model->reservaList();
+        $this->view->carroList = $this->model->carroList();
         $this->view->render('reserva/index');
     }
+    
+     public function selecionaCarro(){
+         
+     }
     
     public function create() 
     {
@@ -19,11 +24,11 @@ class Reserva extends Controller {
         $data['userid'] = $_SESSION['userid'];
         $data['categoria'] = $_POST['categoria'];
         $data['date_added'] = $_POST['date_added'];
-        $data['date_inicio'] = $_POST['date_inicio'];
-        $data['date_fim'] = $_POST['date_fim'];
+        $data['date_inicio'] = $this->model->dateToDB($_POST['date_inicio']);
+        $data['date_fim'] = $this->model->dateToDB($_POST['date_fim']);
         $data['status'] = $_POST['status'];  
-        $data['disponivel'] = 0;
-
+               
+        
         // @TODO: Do your error checking!
         
         $this->model->create($data);
@@ -31,9 +36,10 @@ class Reserva extends Controller {
         
     }
     
-    public function edit($id) 
+    public function edit($reservaid) 
     {
-        $this->view->user = $this->model->reservaSingleList($id);
+        $this->view->reserva = $this->model->reservaSingleList($reservaid);
+        
         $this->view->render('reserva/edit');
     }
     
@@ -59,6 +65,12 @@ class Reserva extends Controller {
     public function delete($id)
     {
         $this->model->delete($id);
+        header('location: ' . URL . 'reserva');
+    }
+    
+    public function deleteCar($id)
+    {
+        $this->model->deleteCar($id);
         header('location: ' . URL . 'reserva');
     }
 }
