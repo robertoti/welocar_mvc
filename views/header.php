f0966c5947851804f9f36545e833104f2ec89790
robertoti <info.robertolima@gmail.com>
2016-09-30 03:12:35 -0300

    Link de plugins para o funcionamento do datepicker

--- views/header.php
+++ views/header.php
@@ -4,11 +4,24 @@
         <title>Welocar</title>
         <link rel="stylesheet" href="<?php echo URL; ?>public/css/default.css" />    
         <link rel="shortcut icon" href="<?php echo URL; ?>public/images/favicon.ico" type="image/ico" />
+        <link href="<?php echo URL; ?>public/jquery-ui/jquery-ui.min.css" rel="stylesheet" type="text/css"/>
+
         <link href="<?php echo URL; ?>public/bootstrap/css/bootstrap.min.css" rel="stylesheet" type="text/css"/>
         <link href="<?php echo URL; ?>public/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css"/>
         <link href="<?php echo URL; ?>public/jquery-modal/jquery.modal.css" rel="stylesheet" type="text/css"/>
         <link href="<?php echo URL; ?>public/owl-carousel/owl-carousel/owl.carousel.css" rel="stylesheet" type="text/css"/>
-        <link href="<?php echo URL; ?>public/owl-carousel/owl-carousel/owl.theme.css" rel="stylesheet" type="text/css"/>
+        <link href="<?php echo URL; ?>public/owl-carousel/owl-carousel/owl.theme.css" rel="stylesheet" type="text/css"/>        
+
+        <!-- jQuery e Datepicker -  Para o funcionamento do  DataPicker da pagina de de reservas  para o carousel  -->
+        
+        <script src="<?php echo URL; ?>public/jquery/jquery.js" type="text/javascript"></script>        
+        <link href="<?php echo URL; ?>public/datepicker/css/datepicker.css" rel="stylesheet" type="text/css"/>
+        <script src="<?php echo URL; ?>public/datepicker/js/bootstrap-datepicker.js" type="text/javascript"></script>
+        <script>
+            $(function () {
+                $("#datepicker").datepicker();
+            });
+        </script>
 
         <?php
         if (isset($this->js)) {
@@ -49,54 +62,54 @@
                         <ul id="float-l" class="list-auto float-l">
 
 
-                    <?php Session::init(); ?>
-                        <li>
-                            <a href="<?php echo URL; ?>index">Inicio</a>
-                        </li>
-                    <?php if (Session::get('loggedIn') == false): ?>
-
-                        <li>
-                            <a href="<?php echo URL; ?>help">Help</a>                               
-                        </li>
-                    <?php endif; ?>
-
-                    <?php if (Session::get('loggedIn') == TRUE): ?>
-                        <li>                                                            
-                            <a href="<?php echo URL; ?>dashboard">Administração</a>
-                        </li>
-                        <li>
-                            <a href="<?php echo URL; ?>reserva">Reservas</a>
-                        </li>
-                        <li>
-                            <a href="<?php echo URL; ?>note">Notas</a>
-                        </li>
-                        <?php if (Session::get('role') == 'owner'): ?>
-                        <li>
-                            <a href="<?php echo URL; ?>user">Usuários</a>                                    
-                        </li>
-                        <?php endif; ?>
+                            <?php Session::init(); ?>
                             <li>
-                                <a href="<?php echo URL; ?>dashboard/logout">Sair</a>    
+                                <a href="<?php echo URL; ?>index">Inicio</a>
                             </li>
-                    <?php else: ?>                              
-                            <li>
-                                <a href="<?php echo URL; ?>login">Login</a>
-                            </li>
-                    <?php endif; ?>    
+                            <?php if (Session::get('loggedIn') == false): ?>
+
+                                <li>
+                                    <a href="<?php echo URL; ?>help">Help</a>                               
+                                </li>
+                            <?php endif; ?>
+
+                            <?php if (Session::get('loggedIn') == TRUE): ?>
+                                <li>                                                            
+                                    <a href="<?php echo URL; ?>dashboard">Administração</a>
+                                </li>
+                                <li>
+                                    <a href="<?php echo URL; ?>reserva">Reservas</a>
+                                </li>
+                                <li>
+                                    <a href="<?php echo URL; ?>note">Notas</a>
+                                </li>
+                                <?php if (Session::get('role') == 'owner'): ?>
+                                    <li>
+                                        <a href="<?php echo URL; ?>user">Usuários</a>                                    
+                                    </li>
+                                <?php endif; ?>
+                                <li>
+                                    <a href="<?php echo URL; ?>dashboard/logout">Sair</a>    
+                                </li>
+                            <?php else: ?>                              
+                                <li>
+                                    <a href="<?php echo URL; ?>login">Login</a>
+                                </li>
+                            <?php endif; ?>    
 
 
 
-                    <?php if (Session::get('loggedIn') == false): ?>
-                        <li>
-                            <a href="<?php echo URL ?>#nossos-veiculos" title="">Nossos veículos</a>  
-                        </li>
-                        <li>
-                            <a href="#contato" title="">Contato</a>                              
-                        </li>                           
+                            <?php if (Session::get('loggedIn') == false): ?>
+                                <li>
+                                    <a href="<?php echo URL ?>#nossos-veiculos" title="">Nossos veículos</a>  
+                                </li>
+                                <li>
+                                    <a href="#contato" title="">Contato</a>                              
+                                </li>                           
 
-                        </ul>
+                            </ul>
 
-                    </nav>
+                        </nav>
 
 
                         <?php if ($_SERVER['REQUEST_URI'] != '/mvc/login'): ?>
