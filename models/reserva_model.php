bd6a212ba07e3b259f1f1795cd4ca6beaedd0f84
robertoti <info.robertolima@gmail.com>
2016-09-30 03:10:06 -0300

    Lista de locações pelo id do usuário; Métodos que buscas e salva as reservas no DB.

--- models/reserva_model.php
+++ models/reserva_model.php
@@ -8,30 +8,60 @@ class Reserva_Model extends Model
         
     }
 
-    public function reservaList()
+    public function reservaListFunc()
     {
         return $this->db->select('SELECT reservaid, userid, car_id,categoria,date_added, date_inicio, date_fim, status FROM reserva;');
     }
-    
+       
+
+    public function reservaList()
+    {
+        
+        $userid = Session::get('userid');
+        
+        return $this->db->select('SELECT reservaid, userid, car_id,categoria,date_added, date_inicio, date_fim, status FROM reserva WHERE userid = :userid', array(':userid' => $userid));
+    }
+       
     public function reservaSingleList($reservaid)
     {
-        return $this->db->select('reservaid, userid, car_id,categoria,date_added, date_inicio, date_fim, status FROM reserva WHERE reservasid = :reservasid', array(':reservasid' => $reservasid));
+        return $this->db->select('SELECT reservaid, userid, car_id, categoria, date_added, date_inicio, date_fim, status FROM reserva WHERE reservaid = :reservaid', array(':reservaid' => $reservaid));
+    }
+
+    public function carroList()
+    {
+        return $this->db->select('SELECT car_id, categoria, disponivel, placa, km FROM carro;');
     }
     
-    public function create($data)
+    public function carroSingleList($car_id)
+    {
+        return $this->db->select('SELECT car_id, categoria, disponivel, placa, km FROM car WHERE car_id = :car_id', array(':car_id' => $car_id));
+    }
+    
+    public function dateToDB($data){
+        
+        $data = implode("-",array_reverse(explode("/",$data)));
+        
+        return $data;
+    }
+    
+    public function selecionaCarro($categoria){
+        
+        return  $this->db->select('SELECT car_id, categoria, disponivel FROM carro WHERE categoria = :categoria');
+        
+    }
+
+        public function create($data)
     {
         $this->db->insert('reserva', array( 
-            'userid' => $_SESSION['userid'],
+            'userid' => $_SESSION['userid'],            
             'categoria' => $data['categoria'],
             'date_added' => $data['date_added'],
             'date_inicio' => $data['date_inicio'],
             'date_fim' => $data['date_fim'],
-            'status' => $data['status'],         
-            'disponivel' => $data['disponivel']
-        ));       
-            
-           
-        var_dump($data);
+            'status' => $data['status']             
+        )); 
+        
+                
     }
     
     public function editSave($data)
@@ -48,16 +78,26 @@ class Reserva_Model extends Model
     
     public function delete($reservaid)
     {
-        $result = $this->db->select('SELECT role FROM reserva WHERE reservaid = :reservaid', array(':reservaid' => $reservaid));
+        $result = $this->db->select('SELECT status FROM reserva WHERE reservaid = :reservaid', array(':reservaid' => $reservaid));
 
         if ($result[0]['status'] == 'ativo')
         return false;
         
         $this->db->delete('reserva', "reservaid = '$reservaid'");
+        
+
     }
-    public function calculaDataEntrega($data_retirada, $quantidadeDias)
+    
+    public function deleteCar($car_id)
     {
-        $data_entrega = strtotime($data_retirada . $quantidadeDias);       
-               
+        $result = $this->db->select('SELECT disponivel FROM reserva WHERE car_id = :car_id', array(':car_id' => $car_id));
+
+        if ($result[0]['status'] == '1')
+        return false;
+        
+        $this->db->delete('carro', "car_id = '$car_id'");
+        
+        
     }
+    
 }
